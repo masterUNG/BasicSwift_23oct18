c12730a846fb988a515e93c60929fcb0da13e15a
masterUNG <phrombutr@gmail.com>
2018-10-23 14:12:32 +0700

    Loop OK

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//For Loop
+let nameStrings = ["AA", "BB", "CC", "DD", "EE"]
+
+for myNameString in nameStrings {
+    print("myName ==> \(myNameString)")
+}
+
+//While Loop
+
+var myNumberInt: Int = 1
+while myNumberInt <= 10 {
+    print("myNumber ==> \(myNumberInt)")
+    myNumberInt += 1
+}
+
+

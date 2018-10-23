ee32f6e16ab7b0c21d32048c3512f87003fa0eca
masterUNG <phrombutr@gmail.com>
2018-10-23 09:48:28 +0700

    Start User Git

---
+++ VarAnConstant.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Variable and Constant
+//การประกาศตัวแปร แบบ Implicit
+var nameString = "MasterUNG"    // นี่คือการประกาศตัวแปรแบบกำหนดค่าด้วย Value
+var numberInt = 123
+
+//Explicit Type
+var surnameString: String = "Phrombutr"
+var number2Int: Int = 555
+var number2Dou: Double = 12.34
+var statusBool: Bool = true
+
+
+
+
+
+
